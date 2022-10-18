d7fedf8f6fa6b4d0af3ec13e5aff9ea62223ad62
mdamjanovic <96126760+damjanovicm@users.noreply.github.com>
2022-10-18 20:30:37 +0200

    Add files via upload

--- portfolio/app.js
+++ portfolio/app.js
@@ -13,10 +13,16 @@ hiddenElem.forEach((el) => observer.observe(el))
 
 
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById('myNav').style.width = "100%";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById('myNav').style.width = "0%";
 }
 
+
+const loader = document.getElementById('preloader')
+
+window.addEventListener('load', function(){
+    loader.style.display = "none"
+})
